1ec136a5d523dd75d435634384da2f08f9926cfb
christina-ngg <64330720+christina-ngg@users.noreply.github.com>
2023-04-18 15:52:49 +0100

    Update streamlit_app.py
    made some changes- added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title("My Parents' New Healthy Diner")
+
+streamlit.header('I am now making changes and seeing what happens to streamlit.')
+                
+                streamlit.text("Mission: identifying whether my parents' diner business model needs to be redesigned to be healthier."
+                
+          
