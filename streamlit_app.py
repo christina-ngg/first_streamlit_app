3208b388bff63c35d3982cbbf473bc93b25ad2a6
christina-ngg <64330720+christina-ngg@users.noreply.github.com>
2023-04-18 16:16:51 +0100

    Update streamlit_app.py
    made changes as per snowflake tutorial

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
 
 streamlit.title("My Parents' New Healthy Diner")
-
-streamlit.header('I am now making changes and seeing what happens to streamlit.')
-                
-streamlit.text("Mission: identifying whether my parents' diner business model needs to be redesigned to be healthier.")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
                 
           
