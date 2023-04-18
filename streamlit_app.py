1dc09b8faa3bbaa3510f35089ccdfcaf73f0434a
christina-ngg <64330720+christina-ngg@users.noreply.github.com>
2023-04-18 16:37:34 +0100

    Update streamlit_app.py
    fruit selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,9 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
 
+
+#select fruit
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 streamlit.dataframe(my_fruit_list)
