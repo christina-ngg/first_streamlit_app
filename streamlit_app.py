01f5970061d9fafa0f135c7d547b74dcd11d03e2
christina-ngg <64330720+christina-ngg@users.noreply.github.com>
2023-04-18 15:58:34 +0100

    Update streamlit_app.py
    got rid of indent

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title("My Parents' New Healthy Diner")
 
 streamlit.header('I am now making changes and seeing what happens to streamlit.')
                 
-                streamlit.text("Mission: identifying whether my parents' diner business model needs to be redesigned to be healthier."
+streamlit.text("Mission: identifying whether my parents' diner business model needs to be redesigned to be healthier."
                 
           
