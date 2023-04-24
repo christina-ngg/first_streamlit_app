64d50da760bbf694677390a4c77b85a42196c2e0
christina-ngg <64330720+christina-ngg@users.noreply.github.com>
2023-04-24 08:45:43 +0100

    Update streamlit_app.py
    added json for watermelon

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,5 +26,6 @@ else:
 
   
 import requests
+streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
